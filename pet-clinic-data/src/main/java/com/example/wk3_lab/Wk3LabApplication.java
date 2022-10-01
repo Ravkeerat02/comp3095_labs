875517bdf1f6d01c0ab7035196a920d853867cf6
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2022-10-01 11:40:07 -0400

    Week 3 lab

---
+++ pet-clinic-data/src/main/java/com/example/wk3_lab/Wk3LabApplication.java
@@ -0,0 +1,13 @@
+package com.example.wk3_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wk3LabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wk3LabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wk3_lab/Wk3LabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wk3_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wk3LabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
